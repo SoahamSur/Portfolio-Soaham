56ce657c685e322745b353b2723876a7982167e6
Soaham Sur <soaham1028messi@gmail.com>
2024-10-20 12:23:30 +0530

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
@@ -12,8 +13,37 @@ import {
   StarsCanvas,
 } from "./components";
 import Footer from "./components/Footer";
+import LoaderSVG from "./components/LoaderSvg"; // Import the LoaderSVG component
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // State to track if loading
+  const [progress, setProgress] = useState(0); // State to track the loading progress
+
+  useEffect(() => {
+    // Simulate a loading progress with a timer
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          clearInterval(interval); // Stop the loading when progress reaches 100%
+          setIsLoading(false); // Hide the loader when loading is complete
+          return 100;
+        }
+        return prevProgress + 10; // Increment the progress by 10
+      });
+    }, 1200); // Update progress every 500ms
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+      {/* Loader container */}
+      <div className="flex items-center justify-center w-[200px] h-[200px]">
+        <LoaderSVG progress={progress} /> {/* Loader SVG with dynamic progress */}
+      </div>
+    </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
